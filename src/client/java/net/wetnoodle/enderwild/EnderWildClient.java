e839f49451ab380648d7f2a96c47fa858c1e9fc7
Ashlyn <kluski42@gmail.com>
2025-05-23 12:07:54 -0400

    Start

---
+++ src/client/java/net/wetnoodle/enderwild/EnderWildClient.java
@@ -0,0 +1,10 @@
+package net.wetnoodle.enderwild;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderWildClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
